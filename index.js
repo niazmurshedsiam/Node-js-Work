71973bb22fcfc96fa156683e94307542e45663f0
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-23 22:01:22 +0600

    api return object, create multiple api

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-    res.send('Thank You')
+    const fruits ={
+        product:'apple',
+        price:300
+
+    }
+    res.send(fruits);
+  })
+  app.get('/fruits/apple',(req,res)=>{
+       res.send({fruits:'apple',quantity:100,price:10000})
   })
   
-app.listen(3000,()=> console.log('Listenting is port 3000'));
+app.listen(4000,()=> console.log('Listenting is port 4000'));
 
