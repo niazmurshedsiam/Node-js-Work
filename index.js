964913aa1885c2607494701a185b5eb0470898d2
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-24 11:28:50 +0600

    use fetch to load data from server, middleware, handle cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const users = ["Abir","Bulbul","Chapman","Dulal"];
+const users = ["Abir","Bulbul","Chapman","Dulal","Fakrul"];
 app.use(cors());
 app.get('/', function (req, res) {
     const fruits ={
